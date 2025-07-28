38c40cd7955781f664e5d9821b79deb4fd8ce333
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-07-28 01:19:15 -0700

    Update DeviceCheckRequest.swift

--- Sources/VaporDeviceCheck/DeviceCheckRequest.swift
+++ Sources/VaporDeviceCheck/DeviceCheckRequest.swift
@@ -3,7 +3,7 @@ import Vapor
 struct DeviceCheckRequest: Content {
     let deviceToken: String
     let transactionId: String = UUID().uuidString
-    let timestamp: Int = Int(Date().timeIntervalSince1970 * 1000)
+    let timestamp: Int = Int(Date().timeIntervalSince1970)
     
     enum CodingKeys: String, CodingKey {
         case deviceToken = "device_token"
